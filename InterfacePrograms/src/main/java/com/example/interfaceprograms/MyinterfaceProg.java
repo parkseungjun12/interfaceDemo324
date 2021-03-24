0e79c93561b5374a9b1a923ff921f0256bbf92ca
박승준 <qtw32@naver.com>
2021-03-24 21:31:32 +0900

    implemented interface

--- InterfacePrograms/src/main/java/com/example/interfaceprograms/MyinterfaceProg.java
+++ InterfacePrograms/src/main/java/com/example/interfaceprograms/MyinterfaceProg.java
@@ -15,21 +15,14 @@ public class MyinterfaceProg {
         System.out.println("Average area: " + average(countries));
     }
 
-    private static double average(Country[] objects) {
+    private static double average(Measurable[] objects) {
         if (objects.length == 0) {return 0;}
         double sum = 0;
-        for (Country obj : objects){
-            sum = sum + obj.getArea();
+        for (Measurable obj : objects){
+            sum = sum + obj.getMeasure();
         }
         return  sum / objects.length;
     }
 
-    private static double average(BankAccount[] objects) {
-        if (objects.length == 0) {return 0;}
-        double sum = 0;
-        for (BankAccount obj : objects){
-            sum = sum + obj.getBalance();
-        }
-        return  sum / objects.length;
-    }
+
 }
